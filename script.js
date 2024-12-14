63ce14f958eccf6bc3cb6fa72c1e12ebbedbb8b8
zuleihah <157889184+Zuleihah@users.noreply.github.com>
2024-12-14 09:35:11 +0100

    make calculator functional

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function add(a,b){
     return +a + +b;
 }
@@ -6,7 +8,7 @@ function subtract(a,b){
     return +a - +b;
 }
 function multiply(a,b){
-    return +a * +b;
+    return +a* +b;
 }
 function divide(a,b){
     return +a/+b;
@@ -30,3 +32,81 @@ function operate(operator, num1, num2){
         break;
     }
 }
+
+const screen = document.querySelector("#screen p")
+const innerScreen = document.querySelector("#screen span")
+
+const digit = document.querySelectorAll(".digit");
+const operation = document.querySelectorAll(".operator")
+
+const equalTo = document.querySelector("#equalTo")
+const erase  = document.querySelector("#clearE")
+const clear = document.querySelector("#clear")
+const minus = document.querySelector("#minus")
+const percent = document.querySelector('#percent')
+
+
+document.querySelectorAll('button').forEach(btn => {
+    btn.addEventListener('mousedown', ()=>{
+        btn.setAttribute('style','opacity:0.5;')
+    })
+    btn.addEventListener('mouseup', ()=> {
+        btn.setAttribute('style','opacity:1;')
+    })
+})
+
+let check;
+digit.forEach(btn => {
+    btn.addEventListener('click', ()=> {
+        if(check){
+            screen.innerHTML= "";
+            check=false;
+            screen.innerHTML += btn.value;
+        }
+        else{
+            screen.innerHTML += btn.value;
+        }  
+    })
+})
+
+operation.forEach(btn => {
+    btn.addEventListener('click', ()=> {
+        num1 = screen.innerHTML;
+        operator = btn.value;
+        innerScreen.innerHTML = screen.innerHTML + btn.value;
+        check=true;
+        ;
+    })
+})
+
+equalTo.addEventListener('click', ()=> {
+    if(check === true){
+        screen.innerHTML = 'invalid format'
+        innerScreen.innerHTML = 'invalid format'
+        screen.setAttribute('style','font-size: 1.5em; line-height: 36px; color: #d7b5b5;')
+        innerScreen.setAttribute('style','font-size: 0.8em; color: #d7b5b5;')
+    }
+    else{
+        num2 = screen.innerHTML;
+        innerScreen.innerHTML += screen.innerHTML + equalTo.value
+        screen.innerHTML = operate(operator,num1,num2)
+    }
+})
+clear.addEventListener('click', ()=> {
+    screen.setAttribute('style','font-size; line-height; color')
+    innerScreen.setAttribute('style','font-size; color;')
+    screen.innerHTML = ""
+    innerScreen.innerHTML=""
+    num1=""; num2=""; operator="";
+})
+
+percent.addEventListener('click', ()=>{
+
+    
+    innerScreen.innerHTML += screen.innerHTML + percent.value
+    screen.innerHTML = +(screen.innerHTML)/100;
+    num1 = screen.innerHTML;
+    console.log(screen.innerHTML, num1)
+
+})
+
